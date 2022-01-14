c3b2bdffb5918265c3a74557ccc46a5d9471f925
islanderian <createdby.island@gmail.com>
2022-01-15 03:24:17 +0900

    lec 7.1 - Todo List

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import { useEffect, useState } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("hi"); // 컴포넌트 생성될 때
-    return () => {
-      console.log("bye"); // 컴포넌트 없어질 때, clean up
-    };
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]); //각 toDo 들을 저장할 array
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault(); // form 의 기본 속성인 onSubmit의 가본속성 없애기(새로고침 안되게)
+    //toDo 가 비어있다면 그냥 return
+    if (toDo === "") {
+      return;
+    }
+    setToDo(""); //submit 하면 input의 value 다시 초기화
+    setToDos((currentArray) => [toDo, ...currentArray]); // 기존 array 에 새로운 toDo 추가! '...[array]' array 배열의 각 elelment를 의미!
+  };
+
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {
+          toDos.map((item, index) => (
+            <li key={index}>{item}</li>
+          )) /*map 은 배열의 item 각각에 함수를 실행해서 item 갯수만큼의 return 값을 반환해서 배열로 만든다  */
+        }
+      </ul>
     </div>
   );
 }
